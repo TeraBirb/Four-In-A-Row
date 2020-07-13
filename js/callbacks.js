81cd1639230c7af2bc9cf592d2723d4d8506f7ca
Isaiah <isaiah.g.callano@gmail.com>
2020-07-12 23:39:26 -0700

    finish ajax requests

--- js/callbacks.js
+++ js/callbacks.js
@@ -1,24 +1,30 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const peopleList = document.getElementById('people');
-const btn = document.querySelector('button');
+const astrosUrl = "http://api.open-notify.org/astros.json";
+const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+const peopleList = document.getElementById("people");
+const btn = document.querySelector("button");
 
 // Make an AJAX request
-function getJSON(url) {
+function getJSON(url, callback) {
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
+  xhr.open("GET", url);
   xhr.onload = () => {
-    if(xhr.status === 200) {
+    if (xhr.status === 200) {
       let data = JSON.parse(xhr.responseText);
-      console.log(data);
+      return callback(data);
     }
   };
   xhr.send();
 }
 
+function getProfiles(json) {
+  json.people.map((person) => {
+    getJSON(wikiUrl + person.name, generateHTML);
+  });
+}
+
 // Generate the markup for each profile
 function generateHTML(data) {
-  const section = document.createElement('section');
+  const section = document.createElement("section");
   peopleList.appendChild(section);
   section.innerHTML = `
     <img src=${data.thumbnail.source}>
@@ -27,3 +33,8 @@ function generateHTML(data) {
     <p>${data.extract}</p>
   `;
 }
+
+btn.addEventListener("click", (e) => {
+  getJSON(astrosUrl, getProfiles);
+  e.target.style.display = "none";
+});

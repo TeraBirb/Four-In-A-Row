4a216b22ee29e5f955e62d3f1012e629181fc8e3
Isaiah <isaiah.g.callano@gmail.com>
2020-07-14 23:31:56 -0700

    finish promises

--- js/promises.js
+++ js/promises.js
@@ -1,38 +1,44 @@
-const astrosUrl = 'http://api.open-notify.org/astros.json';
-const wikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
-const peopleList = document.getElementById('people');
-const btn = document.querySelector('button');
-
-function getJSON(url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = () => {
-    if(xhr.status === 200) {
-      let data = JSON.parse(xhr.responseText);
-      return callback(data);
-    }
-  };
-  xhr.send();
-}
+const astrosUrl = "http://api.open-notify.org/astros.json";
+const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+const peopleList = document.getElementById("people");
+const btn = document.querySelector("button");
 
 function getProfiles(json) {
-  json.people.map( person => {
-    getJSON(wikiUrl + person.name, generateHTML);      
-  }); 
+  const profiles = json.people.map(person => {
+    return fetch(wikiUrl + person.name)
+      .then(response => response.json())
+      .catch(err => console.log("Error fetching Wiki", err))
+  });
+  return Promise.all(profiles);
 }
 
 function generateHTML(data) {
-  const section = document.createElement('section');
-  peopleList.appendChild(section);
-  section.innerHTML = `
-    <img src=${data.thumbnail.source}>
-    <h2>${data.title}</h2>
-    <p>${data.description}</p>
-    <p>${data.extract}</p>
-  `;
+  data.map((person) => {
+    const section = document.createElement("section");
+    peopleList.appendChild(section);
+    section.innerHTML = `
+      <img src=${person.thumbnail.source}>
+      <h2>${person.title}</h2>
+      <p>${person.description}</p>
+      <p>${person.extract}</p>
+    `;
+  });
 }
 
-btn.addEventListener('click', (event) => {
-  getJSON(astrosUrl, getProfiles);
-  event.target.remove();
+btn.addEventListener("click", (event) => {
+  event.target.textContent = "Loading...";
+
+  fetch(astrosUrl)
+    .then((response) => {
+      response.json();
+    })
+    .then(getProfiles)
+    .then(generateHTML)
+    .catch((err) => {
+      peopleList.innerHTML = "<h3>Something went wrong!</h3>";
+      console.log(err);
+    })
+    .finally(() => {
+      event.target.remove();
+    });
 });
